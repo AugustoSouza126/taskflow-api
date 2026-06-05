6c7cecaa280a68f45ee7e7b185cd1b43b845157b
Augusto Souza <augusto281003@gmail.com>
2026-06-04 23:03:19 -0300

    feat: changes corsConfig url and add http

--- src/main/java/com/augusto/taskflow/config/SecurityConfig.java
+++ src/main/java/com/augusto/taskflow/config/SecurityConfig.java
@@ -16,7 +16,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
+
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
